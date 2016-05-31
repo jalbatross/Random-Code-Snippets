a8df7d83fecfe56bd06c9590755ad59f15ea22e6
jalbatross <ih8u2o@gmail.com>
2016-05-31 12:14:14 -0700

    zz

--- Pieson/IterativeSearch.py
+++ Pieson/IterativeSearch.py
@@ -33,5 +33,13 @@ shiftedLongArray = [24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14,
                     13, 12, 11, 10, 9 , 8 , 7, 6, 5, 4, 3, 2, 
                     1, 33, 32, 31, 30, 29, 28, 27, 26, 25];
 
-                    
-print ("The array was shifted ", iterativeP(shiftedLongArray, 33) , " times");
+reallyBigArray = [];
+bigSize = 815315;
+pShift = 392;
+
+for i in range(pShift, 0, -1):
+    reallyBigArray.append(i);
+for i in range (bigSize, pShift + 1, -1):
+    reallyBigArray.append(i);
+
+print ("The array was shifted ", iterativeP(reallyBigArray, len(reallyBigArray)) , " times");
